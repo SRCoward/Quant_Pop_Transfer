f85ccd9301784ee61afeed9cfb60b2e3f396c76e
Samuel Coward <src68@gandy>
2019-02-15 17:05:50 +0000

    include/move the classical simulation folder to git

--- classical_simulation/2_local_evolution.cpp
+++ classical_simulation/2_local_evolution.cpp
@@ -28,14 +28,14 @@ using namespace std;
 }
 */
 //Lets just read in the useful stuff that we actually need as have already generated all the data in python
-void FFT_statevec(int n, vector<complex<long double>> &statevec){
+void FFT_statevec(int n, vector<complex<double>> &statevec){
 	int layer = 1;
 	double normalise = sqrt(pow(2,-n));
 	while (layer<pow(2,n)){
 		for (int i=0;i<pow(2,n);i+=layer*2){
 			for (int j=i;j<i+layer;j++){
-				complex<long double> x = statevec[j];
-				complex<long double> y = statevec[j + layer];
+				complex<double> x = statevec[j];
+				complex<double> y = statevec[j + layer];
 				statevec[j] = x + y;
 				statevec[j+layer] = x - y;
 				if (layer >= pow(2,n-1)-0.01){
@@ -50,7 +50,7 @@ void FFT_statevec(int n, vector<complex<long double>> &statevec){
 //	double divisor = sqrt(1/pow(2,power));
 //	complex& operator*=(statevec,1);
 }
-void read_in_evals(int n,vector<complex<long double>> &exp_classical_evals,vector<complex<long double>> &exp_driver_evals){
+void read_in_evals(int n,vector<complex<double>> &exp_classical_evals,vector<complex<double>> &exp_driver_evals){
 	ifstream inFile("exp_classical_evals.txt");
 	string line;
 	int i = 0;
@@ -58,15 +58,13 @@ void read_in_evals(int n,vector<complex<long double>> &exp_classical_evals,vecto
 		getline(inFile,line);
 		size_t comma = line.find(',');
 //		cout<<line.substr(0,comma-1)<<line.substr(comma+1)<<endl;	
-		long double a = std::stod(line.substr(0,comma-1));
-		long double b = std::stod(line.substr(comma+1)); 
-		complex<long double> ab = {a,b};	
+		double a = std::stod(line.substr(0,comma-1));
+		double b = std::stod(line.substr(comma+1)); 
+		complex<double> ab = {a,b};	
 		exp_classical_evals.push_back(ab);
 		 		
 		i+=1;
-
-		//cout<<"a="<<complex<double>(a,b*10)<<"  "<<exp_classical_evals.at(i-1)<<endl;
-
+//		cout<<"a="<<complex<double>(a,b)<<"  "<<exp_classical_evals.at(i-1)<<endl;
 	}
 	inFile.close();
 	ifstream inFile2("exp_driver_evals.txt");
@@ -75,46 +73,47 @@ void read_in_evals(int n,vector<complex<long double>> &exp_classical_evals,vecto
 		getline(inFile2,line);
 		size_t comma = line.find(',');
 //		cout<<line.substr(0,comma-1)<<line.substr(comma+1)<<endl;	
-		long double a = std::stod(line.substr(0,comma-1));
-		long double b = std::stod(line.substr(comma+1)); 
+		double a = std::stod(line.substr(0,comma-1));
+		double b = std::stod(line.substr(comma+1)); 
 		
-		exp_driver_evals.push_back(complex<long double>(a,b));
+		exp_driver_evals.push_back(complex<double>(a,b));
 		//real(exp_driver_evals[i]) = stod(line.substr(0,comma-1)); 
 		//imag(exp_driver_evals[i]) = stod(line.substr(comma+1,line.end()));
 		i+=1;
-		//cout<<exp_driver_evals[i-1]<<endl;
+//		cout<<exp_driver_evals[i-1]<<endl;
 //		cout<<"a="<<complex<double>(a,b)<<endl;
 	}
 	inFile2.close();
 }
 
 
-void evolve_step(int n,vector<complex<long double>> &statevec,vector<complex<long double>> &exp_classical_evals,vector<complex<long double>> &exp_driver_evals){
+void evolve_step(int n,vector<complex<double>> &statevec,vector<complex<double>> &exp_classical_evals,vector<complex<double>> &exp_driver_evals){
 	FFT_statevec(n,statevec);
 	int vec_length = pow(2,n);
-	vector<complex<long double>> update(vec_length,complex<long double>(0,0));
+	vector<complex<double>> update(vec_length,complex<double>(0,0));
 	double normalise = pow(2,-n);
 	int i,k;
-	//int num_threads = omp_get_num_threads();
-	//double sum_threads[num_threads] = {0};
+	int num_threads = omp_get_num_threads();
+	double sum_threads[num_threads] = {0};
 	int thread_num;
 	#pragma omp parallel for private(i)
 	for (i=0; i<vec_length; i++){
-		vector<complex<long double>> classical_evec(vec_length,complex<long double> (0,0));
+		vector<complex<double>> classical_evec(vec_length,complex<double> (0,0));
 		classical_evec[i] = 1;
 		FFT_statevec(n,classical_evec);
 		for (k=0;k<vec_length;k++){
 			update[i]+=statevec[k]*exp_driver_evals[k]*classical_evec[k];
 		}
 		update[i]*= exp_classical_evals[i];
-		//cout<<update[i];
-		/*
-		cout<<"update value "<<i<<" "<<update[i]<<endl;
-		cout<<" classical_evec= "<<classical_evec[i]<<endl;
-		cout<<" driver_evals= "<<exp_driver_evals[i]<<endl;
-		*/
+		if (real(update[i])>pow(10,11)){
+				cout<<"update value "<<i<<" "<<update[i]<<endl;
+				cout<<"classical_evec "<<classical_evec[i]<<endl;
+				cout<<"driver_evals "<<exp_driver_evals[i]<<endl;
+		}
+	//thread_num = omp_get_thread_num();
+	//sum_threads[thread_num]+=pow(real(update[i]),2)+pow(imag(update[i]),2);
 	}
-	long double sum=0;
+	double sum=0;
 	/*
 	for (int j=0;j<num_threads;j++){
 		sum+=sum_threads[j];
@@ -148,11 +147,11 @@ inFile.close();
 cout<<"input start state num:"<<endl;
 cin>>start_state_num;
 
-vector<complex<long double>> statevec(vec_length,0);
+vector<complex<double>> statevec(vec_length,0);
 statevec.at(start_state_num)=1;
 
-vector<complex<long double>>exp_classical_evals;
-vector<complex<long double>>exp_driver_evals;
+vector<complex<double>>exp_classical_evals;
+vector<complex<double>>exp_driver_evals;
 //exp_driver_evals[0]=complex<double>(1,0);
 //cout<<exp_classical_evals[0]<<"hello!"<<endl;
 read_in_evals(n,exp_classical_evals,exp_driver_evals);
@@ -164,18 +163,13 @@ for (int i=0;i<50;i++){
 };
 
 ofstream outFile("results.csv");
-string round_energy = energies.at(start_state_num);
-round_energy = round_energy.substr(0,7);
-double amps;
-amps = pow(real(statevec[start_state_num]),2)+pow(imag(statevec[start_state_num]),2);
-outFile<<round_energy<<','<<amps<<endl;
 for (size_t i=0 ; i<vec_length ; i++){
-	round_energy = energies.at(i);
+	string round_energy = energies.at(i);
 	round_energy = round_energy.substr(0,7);
+	double amps;
 	amps = pow(real(statevec[i]),2)+pow(imag(statevec[i]),2);
 	outFile<<round_energy<<','<<amps<<endl;
 
 }
-
 outFile.close();
 }
---
+++ classical_simulation/plot_data.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+data = np.loadtxt('results.csv',delimiter=',')
+sum = 0
+"""
+for i in range(0,len(data[:,0])):
+    if data[i][0]==0:
+        sum+=data[i][1]
+
+data = np.append(data,[[0, sum]],axis=0)
+#print(data[:,0])
+"""
+plt.bar(data[:,0],data[:,1],width=0.4)
+plt.bar(data[0,0],data[0,1],width=0.4)
+plt.show()
